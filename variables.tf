efab20f01f078bdeb6f008099746790c1c221807
Jordan Conway <jconway@linuxfoundation.org>
2025-07-23 14:07:19 -0400

    More lint fixes, update to use the correct provider/variables for pytorch foundation
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- variables.tf
+++ variables.tf
@@ -1,13 +1,25 @@
 variable "run_manually" {
-  description = "Flag for manual TF runs"
+  description = "Flag for manual Terraform runs"
   type        = bool
   default     = false
 }
 
 variable "environment_tag" {
+  description = "Mapping of environment names to their corresponding tags"
+    type        = map(string)
   default = {
     dev     = "development"
     prod    = "production"
     staging = "staging"
   }
+}
+
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_app_key" {
+  type      = string
+  sensitive = true
 }
