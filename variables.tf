115c3159b09d16511d9691b6fa1f286b8d2719bb
Jordan Conway <jconway@linuxfoundation.org>
2025-07-23 14:59:57 -0400

    Fix kics warning error
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- variables.tf
+++ variables.tf
@@ -15,11 +15,13 @@ variable "environment_tag" {
 }
 
 variable "datadog_api_key" {
+  description = "Datadog API key"
   type      = string
   sensitive = true
 }
 
 variable "datadog_app_key" {
+  description = "Datadog application key"
   type      = string
   sensitive = true
 }
