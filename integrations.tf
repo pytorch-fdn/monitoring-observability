ce6a1c0d6e50124f6671e98fbf7ce430f647897d
Jordan Conway <jconway@linuxfoundation.org>
2025-07-28 14:34:39 -0400

    Don't use deprecated integration
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- integrations.tf
+++ integrations.tf
@@ -1,9 +1,27 @@
 data "aws_caller_identity" "pytorch" {}
 # Datadog integration for AWS
 # This integration allows Datadog to monitor AWS resources and services.
-resource "datadog_integration_aws" "pytorch" {
-  account_id = data.aws_caller_identity.pytorch.account_id
-  role_name  = "DatadogIntegrationRole"
+resource "datadog_integration_aws_account" "pytorch" {
+  aws_account_id = data.aws_caller_identity.pytorch.account_ida
+  aws_partition  = "aws"
+  aws_regions {
+    include_all = true
+  }
+  auth_config {
+    aws_auth_config_role {
+      role_name = "DatadogIntegrationRole"
+    }
+  }
+  logs_config {
+    lambda_forwarder {}
+  }
+  metrics_config {
+    namespace_filters {}
+  }
+  resources_config {}
+  traces_config {
+    xray_services {}
+  }
 }
 # Datadog integration for Slack
 resource "datadog_integration_slack_channel" "pytorch-infra-alerts" {
