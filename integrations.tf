99831553dab3a09db8863c9bb8e3477c62f9da77
Jordan Conway <jconway@linuxfoundation.org>
2025-07-30 13:07:35 -0400

    Try fixing incident.io integration with double jsonencode (#11)
    * Try fixing incident.io integration with double jsonencode
    
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>
    
    * Use heredoc instead of jsonencode so that we can get LOG_SAMPLE properly
    
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>
    
    * Fix tofu fmt
    
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>
    
    ---------
    
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- integrations.tf
+++ integrations.tf
@@ -50,33 +50,35 @@ resource "datadog_webhook" "lf-incident-io" {
   encode_as = "json"
 
   custom_headers = jsonencode({ "Authorization" = "Bearer ${var.incident_io_bearer}  " })
-  payload = jsonencode({
-    alert_transition  = "$ALERT_TRANSITION",
-    deduplication_key = "$AGGREG_KEY-$ALERT_CYCLE_KEY",
-    title             = "$EVENT_TITLE",
-    description       = "$EVENT_MSG",
-    source_url        = "$LINK",
-    metadata = {
-      id             = "$ID",
-      alert_metric   = "$ALERT_METRIC",
-      alert_query    = "$ALERT_QUERY",
-      alert_scope    = "$ALERT_SCOPE",
-      alert_status   = "$ALERT_STATUS",
-      alert_title    = "$ALERT_TITLE",
-      alert_type     = "$ALERT_TYPE",
-      alert_url      = "$LINK",
-      alert_priority = "$ALERT_PRIORITY",
-      date           = "$DATE",
-      event_type     = "$EVENT_TYPE",
-      hostname       = "$HOSTNAME",
-      last_updated   = "$LAST_UPDATED",
-      logs_sample    = "$LOGS_SAMPLE",
-      org = {
-        id   = "$ORG_ID",
-        name = "$ORG_NAME"
-      },
-      snapshot_url = "$SNAPSHOT",
-      tags         = "$TAGS"
-    }
-  })
+  payload        = <<-EOT
+{
+  "alert_transition": "$ALERT_TRANSITION",
+  "deduplication_key": "$AGGREG_KEY-$ALERT_CYCLE_KEY",
+  "title": "$EVENT_TITLE",
+  "description": "$EVENT_MSG",
+  "source_url": "$LINK",
+  "metadata": {
+    "id": "$ID",
+    "alert_metric": "$ALERT_METRIC",
+    "alert_query": "$ALERT_QUERY",
+    "alert_scope": "$ALERT_SCOPE",
+    "alert_status": "$ALERT_STATUS",
+    "alert_title": "$ALERT_TITLE",
+    "alert_type": "$ALERT_TYPE",
+    "alert_url": "$LINK",
+    "alert_priority": "$ALERT_PRIORITY",
+    "date": "$DATE",
+    "event_type": "$EVENT_TYPE",
+    "hostname": "$HOSTNAME",
+    "last_updated": "$LAST_UPDATED",
+    "logs_sample": $LOGS_SAMPLE,
+    "org": {
+      "id": "$ORG_ID",
+      "name": "$ORG_NAME"
+    },
+    "snapshot_url": "$SNAPSHOT",
+    "tags": "$TAGS"
+  }
+}
+EOT
 }
