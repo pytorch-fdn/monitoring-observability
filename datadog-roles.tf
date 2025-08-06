34ff1390cf2a7ef5ebf97ee2bf06d675f1dc0db5
Jordan Conway <jconway@linuxfoundation.org>
2025-08-06 15:53:03 -0400

    Fix more hallucinated permissions
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- datadog-roles.tf
+++ datadog-roles.tf
@@ -25,7 +25,6 @@ variable "dd_roles" {
 
         # Additional write permissions
         "dashboards_write",
-        "notebooks_write",
         "monitors_write",
         "synthetics_write",
         "incidents_write",
