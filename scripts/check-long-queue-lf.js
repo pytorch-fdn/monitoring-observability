bce934eb96b1963f983bfcff52ca902f46e59cb3
Thanh Ha <thanh.ha@linuxfoundation.org>
2025-08-28 11:22:22 -0400

    Increase alert thresholds
    ROCM alerts increase to 4 hrs based on historical HUD data that
    most ROCM queues resolve within 4 hrs time.
    
    Remaining changes increase to 3 hrs based on what we are seeing
    alerted in the #pytorch-infra-alerts channel most of our alerts
    seem to resolve within 3 hrs on their own.
    
    This should hopefully make things less noisy while still alerting
    us when something is unusually high.
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- scripts/check-long-queue-lf.js
+++ scripts/check-long-queue-lf.js
@@ -5,7 +5,7 @@ const jsonData = dd.response.body;
 const parsedData = JSON.parse(jsonData);
 
 const highQueueItems = parsedData
-  .filter(item => item.machine_type.startsWith(MACHINE_TYPE_FILTER) && item.avg_queue_s > 7200)
+  .filter(item => item.machine_type.startsWith(MACHINE_TYPE_FILTER) && item.avg_queue_s > 10800)
   .map(item => ({ machine_type: item.machine_type, avg_queue_s: item.avg_queue_s }));
 
 if (highQueueItems.length > 0) {
