a928f56b0792aee10d1e9e9ff531e844593d4b71
Thanh Ha <thanh.ha@linuxfoundation.org>
2025-09-03 10:06:03 -0400

    Do not fail check if PyTorch HUD API is down (#37)
    If the PyTorch HUD is down we are not able to check on queue details
    so rather than reporting a potentially incorrect alert. Ignore the
    check until the PyTorch HUD API is back in service.
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- scripts/check-long-queue-s390x.js
+++ scripts/check-long-queue-s390x.js
@@ -1,19 +1,24 @@
-dd.expect(dd.response.statusCode).to.equal(200);
-
-const MACHINE_TYPE_FILTER = '.s390x';
-const jsonData = dd.response.body;
-const parsedData = JSON.parse(jsonData);
-
-const highQueueItems = parsedData
-  .filter(item => item.machine_type.includes(MACHINE_TYPE_FILTER) && item.avg_queue_s > 10800)
-  .map(item => ({ machine_type: item.machine_type, avg_queue_s: item.avg_queue_s }));
-
-if (highQueueItems.length > 0) {
-  const machineDetails = highQueueItems
-    .map(item => `${item.machine_type} (${item.avg_queue_s}s)`)
-    .join(', ');
-  const message = `High queue detected for machine types containing ${MACHINE_TYPE_FILTER}: ${machineDetails}`;
-  console.error(message);
+if (dd.response.statusCode !== 200) {
+  // We do not want to fail due to hud.pytorch.org API failure.
+  console.log('Status code is not 200, stopping execution');
+  dd.expect(true).to.equal(true);
 }
+else {
+  const MACHINE_TYPE_FILTER = '.s390x';
+  const jsonData = dd.response.body;
+  const parsedData = JSON.parse(jsonData);
 
-dd.expect(highQueueItems.length > 0).to.be.false;
+  const highQueueItems = parsedData
+    .filter(item => item.machine_type.includes(MACHINE_TYPE_FILTER) && item.avg_queue_s > 10800)
+    .map(item => ({ machine_type: item.machine_type, avg_queue_s: item.avg_queue_s }));
+
+  if (highQueueItems.length > 0) {
+    const machineDetails = highQueueItems
+      .map(item => `${item.machine_type} (${item.avg_queue_s}s)`)
+      .join(', ');
+    const message = `High queue detected for machine types containing ${MACHINE_TYPE_FILTER}: ${machineDetails}`;
+    console.error(message);
+  }
+
+  dd.expect(highQueueItems.length > 0).to.be.false;
+}
